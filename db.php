07a7194c00b18cf10272a39017d01ed49d641996
Anjali Gupta <168294017+anjali-me@users.noreply.github.com>
2025-07-14 09:23:08 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php 
-$con = new mysqli('localhost','root','','online-auction-master.');      
+$con = new mysqli('localhost','root','','online-auction-master');      
 date_default_timezone_set('asia/kolkata');   
  ?>
