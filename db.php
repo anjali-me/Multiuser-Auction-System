77fb955892d801e4a70b5b9a29c37ebbd3b7b7a6
Anjali Gupta <168294017+anjali-me@users.noreply.github.com>
2025-07-14 09:16:27 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php 
-$con = new mysqli('localhost','root','','online-auction-master');       //this is used to connect the database to server 
+$con = new mysqli('localhost','root','','online-auction-master');      
 date_default_timezone_set('asia/kolkata');   
  ?>
